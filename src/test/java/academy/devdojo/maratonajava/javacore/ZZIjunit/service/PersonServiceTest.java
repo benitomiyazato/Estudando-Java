ced09bfabc9269cdffcd84f01908237505f12cb9
bemiyaz <benito.miya172@gmail.com>
2022-08-31 15:16:43 -0300

    @DisplayName annotation in PersonService test class

--- src/test/java/academy/devdojo/maratonajava/javacore/ZZIjunit/service/PersonServiceTest.java
+++ src/test/java/academy/devdojo/maratonajava/javacore/ZZIjunit/service/PersonServiceTest.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.javacore.ZZIjunit.service;
 
 import academy.devdojo.maratonajava.javacore.ZZIjunit.domain.Person;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class PersonServiceTest {
 
     @Test
+    @DisplayName("A person shouldn't be adult when age is lower than 18")
     void isAdult_ReturnFalse_WhenAgeIsLowerThan18() {
         Person person = new Person(15);
         Assertions.assertFalse(PersonService.isAdult(person));
